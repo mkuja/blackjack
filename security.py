7d70f61c206fd77e424326286369aeadcda58e70
Mikko Kujala <m.kujala@live.com>
2020-02-10 23:03:13 +0200

    hjälp

--- security.py
+++ security.py
@@ -1,12 +1,14 @@
-from models.user import User
+from .models.user import User
 from werkzeug.security import safe_str_cmp
 
 
-def authenticate(user: str, password: str) -> User:
+def authenticate(user: str, password: str):
+    raise Exception("authenticate")
     _user = User.find_by_username(user)
-    if _user and safe_str_cmp(User.find_by_username(user).password, password):
+    if _user.username and _user.password == password:
         return _user
 
 def identity(payload):
+    raise Exception("identity")
     user_id = payload['identity']
-    return User.find_by_id(user_id).first()
+    return User.find_by_id(user_id)
